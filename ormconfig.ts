34f5cbe8b8315afc2eb98fe64065711c51d67f08
hsh0340 <hsh0340@naver.com>
2024-04-17 16:54:19 +0900

    fix: ormconfig.ts typeorm 0.3버전에 맞게 수정

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,5 +1,7 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config({ path: '.env' });
 
 const env = process.env.NODE_ENV;
 const host = env === 'test' ? process.env.TEST_DB_HOST : process.env.DB_HOST;
@@ -11,7 +13,7 @@ const password =
 const database =
   env === 'test' ? process.env.TEST_DB_DATABASE : process.env.DB_DATABASE;
 
-module.exports = {
+export const dataSource = new DataSource({
   type: 'mysql' /* 사용할 데이터베이스 종류 */,
   host: host /* 데이터베이스 서버의 호스트 이름 또는 IP 주소 */,
   port: Number(port) /* 데이터베이스 서버의 포트 번호 */,
@@ -21,12 +23,6 @@ module.exports = {
   entities: [
     'apps/**/*.entity.ts',
   ] /* 데이터베이스의 테이블과 매핑될 엔티티 클래스 파일의 위치 */,
-  migrations: ['db/migrations/*.ts'] /* 마이그레이션 파일의 위치 */,
-  cli: {
-    /* CLI 명령 설정 */
-    migrationsDir: 'db/migrations' /* 마이그레이션 디렉토리 위치 */,
-  },
-  seeds: ['apps/**/*.seed.ts'] /* 시드 파일 위치 */,
-  factories: ['apps/**/*.factory.ts'] /* 팩토리 파일 위치 */,
+  migrations: ['./db/migrations/*.ts'] /* 마이그레이션 파일의 위치 */,
   timezone: 'Z' /* 데이터베이스에서 사용할 시간대 */,
-};
+});
