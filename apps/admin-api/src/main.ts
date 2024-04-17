b84edf020485483fc5cd061f07d113fa34a1ccf9
hsh0340 <hsh0340@naver.com>
2024-04-17 17:34:18 +0900

    config: main.ts swagger 설정

--- apps/admin-api/src/main.ts
+++ apps/admin-api/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'apps/admin-api/src/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('[nestjs-monorepo-boilerplate] admin-api')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 
   if (module.hot) {
