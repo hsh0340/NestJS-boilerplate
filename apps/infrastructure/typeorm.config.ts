92d2553dc54786d161cad7b217b215f393ba5c41
hsh0340 <hsh0340@naver.com>
2024-04-18 15:55:57 +0900

    fix: typeorm config에서 configService 사용하지 않도록 수정

--- apps/infrastructure/typeorm.config.ts
+++ apps/infrastructure/typeorm.config.ts
@@ -1,16 +1,14 @@
-import { ConfigService } from '@nestjs/config';
-
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-function typeormConfig(configService: ConfigService): TypeOrmModuleOptions {
+function typeormConfig(): TypeOrmModuleOptions {
   const config =
-    configService.get('NODE_ENV') === 'test'
+    process.env.NODE_ENV === 'test'
       ? {
-          host: configService.get('TEST_DB_HOST'),
-          port: configService.get<number>('process.env.TEST_DB_PORT'),
-          username: configService.get('TEST_DB_USERNAME'),
-          password: configService.get('TEST_DB_PASSWORD'),
-          database: configService.get('TEST_DB_DATABASE'),
+          host: process.env.TEST_DB_HOST,
+          port: Number(process.env.TEST_DB_PORT),
+          username: process.env.TEST_DB_USERNAME,
+          password: process.env.TEST_DB_PASSWORD,
+          database: process.env.TEST_DB_DATABASE,
           // 테스트코드 실행 중 clearDB 하는 과정에서
           // 2개 이상의 커넥션이 생성되는 것을 막기 위해
           // 커넥션 제한
@@ -19,13 +17,12 @@ function typeormConfig(configService: ConfigService): TypeOrmModuleOptions {
           },
         }
       : {
-          host: configService.get('DB_HOST'),
-          port: configService.get<number>('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_DATABASE'),
+          host: process.env.DB_HOST,
+          port: Number(process.env.DB_PORT),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
         };
-
   return {
     type: 'mysql',
     ...config,
@@ -34,9 +31,7 @@ function typeormConfig(configService: ConfigService): TypeOrmModuleOptions {
       decimalNumbers: true,
     },
     autoLoadEntities: true,
-    synchronize:
-      configService.get('SYNC') === 'true' &&
-      configService.get('NODE_ENV') !== 'test',
+    synchronize: process.env.SYNC === 'true' && process.env.NODE_ENV !== 'test',
     dropSchema: false,
     timezone: 'Z',
   };
