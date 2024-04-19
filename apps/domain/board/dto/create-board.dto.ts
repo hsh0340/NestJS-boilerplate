606d6923605615928ac1c21a60045b434aa0de98
hsh0340 <hsh0340@naver.com>
2024-04-19 22:44:03 +0900

    feat: swagger ApiProperty 작성

--- apps/domain/board/dto/create-board.dto.ts
+++ apps/domain/board/dto/create-board.dto.ts
@@ -1,9 +1,18 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBoardDto {
+  @ApiProperty({
+    description: '게시물 제목',
+    example: '게시물 제목 예시',
+  })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    description: '게시물 내용',
+    example: '게시물 내용 예시',
+  })
   @IsString()
   content: string;
 }
