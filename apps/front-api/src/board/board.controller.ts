29e873bd6c58f7cbc7627c32b7f30704571b41f2
hsh0340 <hsh0340@naver.com>
2024-04-19 22:44:31 +0900

    docs: swagger ApiOperation 작성

--- apps/front-api/src/board/board.controller.ts
+++ apps/front-api/src/board/board.controller.ts
@@ -1,10 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { BoardService } from 'apps/domain/board/board.service';
 import { CreateBoardDto } from 'apps/domain/board/dto/create-board.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
+
+  @ApiOperation({
+    operationId: 'createBoard',
+    summary: '게시물 생성',
+    tags: ['board'],
+  })
   @Post('board')
   async createBoard(@Body() dto: CreateBoardDto) {
     return await this.boardService.createBoard(dto);
