6ccc0923e4ce8c3adff2e336a0b334afb5bc1678
hsh0340 <hsh0340@naver.com>
2024-04-19 21:33:51 +0900

    config: 의존성 주입

--- apps/domain/domain.module.ts
+++ apps/domain/domain.module.ts
@@ -1,10 +1,13 @@
 import { BoardService } from 'apps/domain/board/board.service';
 import { Module } from '@nestjs/common';
+import { BoardRepository } from 'apps/domain/board/board.repository';
 
 const services = [BoardService];
 
+const repositories = [BoardRepository];
+
 @Module({
-  providers: [...services],
-  exports: [...services],
+  providers: [...services, ...repositories],
+  exports: [...services, ...repositories],
 })
 export class DomainModule {}
--- apps/front-api/src/controller.module.ts
+++ apps/front-api/src/controller.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardController } from 'apps/front-api/src/board/board.controller';
+import { DomainModule } from 'apps/domain/domain.module';
 
 @Module({
+  imports: [DomainModule],
   controllers: [BoardController],
 })
 export class ControllerModule {}
